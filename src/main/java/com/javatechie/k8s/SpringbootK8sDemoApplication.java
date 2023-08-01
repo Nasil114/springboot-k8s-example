5c85c8d16f79b366330052b4c4f009e326715693
Nasil114 <120000998+Nasil114@users.noreply.github.com>
2023-08-01 15:06:59 +0530

    Update SpringbootK8sDemoApplication.java

--- src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringbootK8sDemoApplication {
 
 	@GetMapping("/message")
 	public String displayMessage(){
-		return "Congratulation you successfully deployed your application to kubernetes !!";
+		return "u are my bestfriend , love u so much!!";
 	}
 
 	public static void main(String[] args) {
